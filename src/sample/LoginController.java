c2518ab4857b13b96254b639b373f1dfc0289e6a
aleemalibhai <aleem.alibhai@gmail.com>
2018-04-01 03:15:48 -0400

    login controller added

---
+++ src/sample/LoginController.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    public void Login(){
+
+    }
+
+    @FXML
+    public void createUserScene(){
+
+    }
+
+    public void initialize(){
+
+    }
+}
